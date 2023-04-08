08b44c324fc23cac2b2df74ad509914a24f8c871
Elena Nikiforova <vizualnoe@gmail.com>
2023-04-08 10:50:54 +0200

    added Docker compose file and fixed cors

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'kpd-backend',
-      script: './dist/main.js',
+      script: './dist/src/main.js',
     },
   ],
 };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-//import config from '../config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors({
-    origin: [
-      'https://kupipodaridai.nomoredomains.monster/',
-      'http://kupipodaridai.nomoredomains.monster/',
-    ],
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-  await app.listen(3001);
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
